2ae88f1a4f35c767349d0158d20cf7d0a7a1580b
Raphael Camilo <raphael332244@gmail.com>
2022-01-04 22:26:56 -0300

    added categories query

--- api/apollo/dataSource/game.js
+++ api/apollo/dataSource/game.js
@@ -20,6 +20,52 @@ class GamesAPI extends RESTDataSource {
       return game;
     });
   }
+
+  async getGame(id) {
+    const game = await this.get(`/games/${id}`);
+    const newCategories = [];
+
+    for (let i = 0; i < game.categories.length; i++) {
+      const category = await this.get(`/categories/${game.categories[i]}`);
+      newCategories.push(category);
+    }
+
+    game.categories = newCategories;
+    return game;
+  }
+
+  async getCategories() {
+    const categories = await this.get(`/categories`);
+    return categories.map(async (cat) => {
+      const games = await this.get("/games");
+      const filteredGames = games.filter((game) =>
+        game.categories.includes(cat.id)
+      );
+      const newGames = [];
+
+      filteredGames.map((game) => {
+        const newCategories = [];
+
+        for (let i = 0; i < game.categories.length; i++) {
+          const category = [...categories];
+
+          newCategories.push(
+            category.filter((cat) => cat.id === game.categories[i])[0]
+          );
+        }
+
+        newGames.push({ ...game, categories: newCategories });
+      });
+
+      return {
+        data: {
+          id: cat.id,
+          type: cat.type,
+        },
+        games: newGames,
+      };
+    });
+  }
 }
 
 module.exports = GamesAPI;
--- api/apollo/resolver/gameResolver.js
+++ api/apollo/resolver/gameResolver.js
@@ -2,6 +2,10 @@ const gameResolvers = {
   Query: {
     games: (parent, args, { dataSources }, info) =>
       dataSources.gamesAPI.getGames(),
+    game: (parent, { id }, { dataSources }, info) =>
+      dataSources.gamesAPI.getGame(id),
+    categories: (parent, args, { dataSources }, info) =>
+      dataSources.gamesAPI.getCategories(),
   },
 };
 
